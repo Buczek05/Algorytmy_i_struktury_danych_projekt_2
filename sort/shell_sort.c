a8a248b4ed773d6fdb91d6fc13e8d3605b7430e6
Kacper Bukowski <kacperbuko2005@gmail.com>
2024-05-05 14:53:36 +0200

    add diagrams

--- sort/shell_sort.c
+++ sort/shell_sort.c
@@ -5,11 +5,10 @@ void shell_sort(int table[], int size) {
         for (i = interval; i < size; i += 1) {
             temp = table[i];
 
-            for (j = i; j >= interval && table[j - interval] > temp; j -= interval) {
+            for (j = i; j >= interval && table[j - interval] > temp;  ) {
                 table[j] = table[j - interval];
             }
 
-            table[j] = temp;
         }
     }
 }
